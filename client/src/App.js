b7924044082360a9647323043f2c95ab053d4eae
shuvosonjoy <shuvosdg123@gmail.com>
2024-10-08 13:06:20 +0600

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Login from './component/accounts/login';
+
+function App() {
+  return (
+    <div className="App" style={{marginTop:'64px'}}>
+<Login/>
+    </div>
+  );
+}
+
+export default App;
