64e4c77f2d58dece9ba258afa17900e907064ed0
shuvosonjoy <shuvosdg123@gmail.com>
2024-11-01 16:45:30 +0600

    post

---
+++ server/controller/post-controller.js
@@ -0,0 +1,14 @@
+
+import Post from '../model/post.js';
+
+
+export const createPost = async (request, response) => {
+    try {
+        const post = await new Post(request.body);
+        post.save();
+
+        response.status(200).json('Post saved successfully');
+    } catch (error) {
+        response.status(500).json(error);
+    }
+}
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { SignupUser,loginUser } from '../controller/user_controller.js';
-import { uploadImage } from '../controller/upload_image.js';
-// api.js
+import { uploadImage,getImage } from '../controller/upload_image.js';
+
 import upload from '../utils/upload.js';
+import { createPost } from '../controller/post-controller.js';
 
 
 
@@ -11,6 +12,12 @@ const router = express.Router();
 
 router.post('/signup',SignupUser);
 router.post('/login',loginUser);
+
+router.post('/create',createPost);
+
 router.post('/file/upload',upload.single('file'),uploadImage);
+router.get('/file/:filename', getImage);
+
+
 
 export default router;
